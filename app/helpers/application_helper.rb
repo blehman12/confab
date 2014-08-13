2d33c7995fc7404fb12071e6705a267b4ca0b2ec
JulesVan <javanwyhe@gmail.com>
2014-08-13 07:23:27 -0700

    fixed title to reflect version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Confab7 Demo App"
+    base_title = "Confab8 Demo App"
     if page_title.empty?
       base_title
     else
