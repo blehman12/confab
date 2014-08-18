07b32738f992b7435501d4929a1703c226a4a2de
JulesVan <javanwyhe@gmail.com>
2014-08-18 15:47:36 -0700

    trying to fix heroku home page bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   def home
     if signed_in?
-      @communication  = current_user.communications.build
-      @commlist_items = current_user.commlist.paginate(page: params[:page])
       @want = current_user.wants.build
       @wantlist_items = current_user.wantlist.paginate(page: params[:page])
       @skill = current_user.skills.build
