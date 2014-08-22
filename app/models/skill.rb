9dee388069de7280956dfce06fe74f9c8cf41dc2
JulesVan <javanwyhe@gmail.com>
2014-08-22 11:26:35 -0700

    made skill level and want priority into drop downs for 1-10

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,6 @@ class Skill < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :skill, presence: true, length: { maximum: 140 }
+
+  LEVEL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 end
--- app/models/want.rb
+++ app/models/want.rb
@@ -2,4 +2,6 @@ class Want < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :wanted, presence: true, length: { maximum: 140 }
+
+  PRIORITY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 end
